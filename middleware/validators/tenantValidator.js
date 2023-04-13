1bcd488c42e1355ebd873763d550f443bfcd7583
nicodav28 <esteveznicolas0@gmail.com>
2023-04-12 21:19:45 -0500

    🔥 refactor(tenantValidator.js): eliminar comentarios innecesarios Se eliminaron los comentarios innecesarios que estaban en el archivo tenantValidator.js. Estos comentarios no aportaban información útil y solo ocupaban espacio en el archivo.

--- middleware/validators/tenantValidator.js
+++ middleware/validators/tenantValidator.js
@@ -3,24 +3,24 @@ const { validateResults } = require('../../utils/helpers');
 const { tenantsModel } = require('../../models/index');
 
 const createAdminValidation = [
-    // check(`payment_id`)
-    //     .isMongoId()
-    //     .withMessage({
-    //         traceCode: 'C-101',
-    //         message: `Ups!, there was an error processing the data, try it again or contact the support team.`
-    //     }),
-    // check(`subscription_id`)
-    //     .isMongoId()
-    //     .withMessage({
-    //         traceCode: 'C-101',
-    //         message: `Ups!, there was an error processing the data, try it again or contact the support team.`
-    //     }),
-    // check(`template_id`)
-    //     .isMongoId()
-    //     .withMessage({
-    //         traceCode: 'C-101',
-    //         message: `Ups!, there was an error processing the data, try it again or contact the support team.`
-    //     }),
+    check(`payment_id`)
+        .isMongoId()
+        .withMessage({
+            traceCode: 'C-101',
+            message: `Ups!, there was an error processing the data, try it again or contact the support team.`
+        }),
+    check(`subscription_id`)
+        .isMongoId()
+        .withMessage({
+            traceCode: 'C-101',
+            message: `Ups!, there was an error processing the data, try it again or contact the support team.`
+        }),
+    check(`template_id`)
+        .isMongoId()
+        .withMessage({
+            traceCode: 'C-101',
+            message: `Ups!, there was an error processing the data, try it again or contact the support team.`
+        }),
     check(`tenant`)
         .exists()
         .notEmpty(),
@@ -79,15 +79,15 @@ const createAdminValidation = [
         .notEmpty()
         .trim()
         .escape(),
-    // check(`subscription`)
-    //     .exists()
-    //     .notEmpty(),
-    // check(`subscription.subscription_date`)
-    //     .exists()
-    //     .notEmpty(),
-    // check(`subscription.subscription_renovationn_date`)
-    //     .exists()
-    //     .notEmpty(),
+    check(`subscription`)
+        .exists()
+        .notEmpty(),
+    check(`subscription.subscription_date`)
+        .exists()
+        .notEmpty(),
+    check(`subscription.subscription_renovationn_date`)
+        .exists()
+        .notEmpty(),
 
 
     (req, res, next) => validateResults(req, res, next)
